052098f5bb13fa8c63a7d0a8cacb7c03f24b66c5
juliah-byte <79874878+juliah-byte@users.noreply.github.com>
2021-12-06 13:00:34 -0500

    Added Deletion Feature

--- src/main/java/com/skillstorm/servlets/FlightServlet.java
+++ src/main/java/com/skillstorm/servlets/FlightServlet.java
@@ -13,15 +13,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.connector.Response;
+
 import com.skillstorm.beans.Flight;
 import com.skillstorm.dao.FlightsDAO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skillstorm.service.FlightService;
 
 
-
 @WebServlet("/api/flight")
 public class FlightServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
 	
 	FlightService serv = new FlightService();
 	FlightsDAO dao = new FlightsDAO();
@@ -34,10 +36,12 @@ public class FlightServlet extends HttpServlet{
 			String json = new ObjectMapper().writeValueAsString(flight);
 			resp.getWriter().print(json);
 			resp.setStatus(200);
+			resp.setContentType("application/json");
 		}else {
 			Set<Flight> flights = dao.findAll();
 			String json = new ObjectMapper().writeValueAsString(flights);
 			resp.getWriter().print(json);
+			resp.setContentType("application/json");
 		}
 	}
 	
@@ -72,15 +76,32 @@ public class FlightServlet extends HttpServlet{
 	
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//String clientOrigin = req.getHeader("origin");
+		
 		if(req.getParameter("id") != null) {
 			String param = req.getParameter("id");
 			int id = Integer.parseInt(param);
 			serv.deleteFlight(id);
+//			resp.setContentType("application/json");
+//			resp.setCharacterEncoding("UTF-8");
+//			resp.addHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+//			resp.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+//			resp.addHeader("Access-Control-Max-Age", "1728000");
+//			resp.addHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//			resp.setHeader("Access-Control-Allow-Origin", "*");
+//			resp.setHeader("Access-Control-Allow-Methods", "DELETE");
+//			resp.setHeader("Access-Control-Allow-Origin", "Content-Type");
+//			resp.setHeader("Access-Control-Max-Age", "86400");
 			resp.setStatus(200);
+//			resp.setContentType("application/json");
 		}else {
 			System.out.println("The entered flight id does not exist.");
 			resp.setStatus(400);
+			resp.setContentType("application/json");
 		}
 	}
 	
+	
+
 }
